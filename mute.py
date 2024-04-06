1d46f224b093a91b63c4275cc2427465c519a261
xan-asc <119889093+Xan-Asc@users.noreply.github.com>
2024-04-05 22:21:11 -0700

    Iterate over data object.  desired result might not be the first one.

--- mute.py
+++ mute.py
@@ -1,5 +1,5 @@
 #
-# Simple program to generate a sequence of 1 second long silent files based on a supplied list
+# Simple program to generate a sequence of silent files based on a supplied list
 # This script requires ffmpeg to be available
 #
 
@@ -15,19 +15,19 @@ def main():
 		'User-Agent': 'mutesound',
 	}
 	requester = requests.Session()
-	for snd in sounds:
+	for c, snd in enumerate(sounds, start=1):
+		print(f"Requesting: {snd} ({c} of {len(sounds)})")
 		url = f'https://wow.tools/files/scripts/api.php?draw=38&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=25&search%5Bvalue%5D=${snd}%2Ctype%3Aogg&search%5Bregex%5D=false&_=1695962035525'
 		req = requester.get(url, headers=header)
-		data = req.json()['data'][0][1]
-		outpath = os.path.join(*(data[0].upper() + data[1:]).split('/'))
-		outfile = pathlib.Path(outpath)
-		outfile.parent.mkdir(exist_ok=True, parents=True)
-		song = AudioSegment.silent(duration=0)
-		song.export(outpath[:-3] + "wav", format="wav")
-		with open(outpath[:-3] + "ogg", 'w') as _:
-			pass
-#		if outpath.endswith("ogg"):
-#			song.export(outpath, format="ogg")
+		for obj in req.json()['data']:
+			data = obj[1]
+			outpath = os.path.join(*(data[0].upper() + data[1:]).split('/'))
+			outfile = pathlib.Path(outpath)
+			outfile.parent.mkdir(exist_ok=True, parents=True)
+			song = AudioSegment.silent(duration=0)
+			song.export(outpath[:-3] + "wav", format="wav")
+			with open(outpath[:-3] + "ogg", 'w') as _:
+				pass
 	return
 
 if __name__ == '__main__':
